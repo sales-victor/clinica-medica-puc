512a9ab3418fe5d0f1fc3ca384188317b976ca9c
Joao Sales <salesvictor@icloud.com>
2023-10-28 11:05:04 -0300

    implementação swagger

--- src/main/java/com/example/demo/controller/ProcedimentoRestCtr.java
+++ src/main/java/com/example/demo/controller/ProcedimentoRestCtr.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,7 +37,7 @@ public class ProcedimentoRestCtr {
 		return procedimentoService.gravarConsulta(paramsConsultaDTO);	
 	}
 	
-	@PostMapping("/consulta/update")
+	@PutMapping("/consulta/update")
 	public Retorno editarConsulta(@RequestBody ParamsConsultaDTO paramsConsultaDTO) {
 		return procedimentoService.editarConsulta(paramsConsultaDTO);	
 	}
@@ -59,6 +60,11 @@ public class ProcedimentoRestCtr {
 	@PostMapping("/exame/create")
 	public Retorno gravarExame(@RequestBody ParamsExameDTO paramsExameDTO) {
 		return procedimentoService.gravarExame(paramsExameDTO);	
+	}
+	
+	@PutMapping("/exame/update")
+	public Retorno editarExame(@RequestBody ParamsExameDTO paramsExameDTO) {
+		return procedimentoService.editarExame(paramsExameDTO);	
 	}
 	
 	@DeleteMapping("/exame/delete/{codExame}")
